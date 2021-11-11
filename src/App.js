1bad9e633a6869cf1e21099893887476e29fdf30
Chris Nelson <cnelson04@gmail.com>
2021-11-10 19:39:01 -0600

    material-ui intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { TextField, Button, Select, MenuItem, InputLabel } from '@material-ui/core';
+import {useState} from 'react'
+
+
 
 function App() {
+
+  const getName = (event) => {
+    console.log(event.target.value);
+  }
+  
+  const getSize = (event) => {
+    console.log('in getSize', event.target.value );
+    setSize(event.target.value)
+  }
+
+  const [size, setSize ] = useState ('small');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Material UI Intro</h1>
+      <br></br>
+      <input type="text" placeholder ="oldSchool" onChange={ (event ) => getName (event )} ></input>
+      <br></br>
+      <TextField placeholder ="newSchool" onChange={ (event ) => getName (event )} ></TextField>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button>oldSchool</button><p>   </p>
+      <Button>newSchool</Button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <InputLabel>Select Size</InputLabel>
+      <Select
+        value={size}
+        onChange={getSize}
+      >
+        <MenuItem value="tiny"> Tiny </MenuItem>
+        <MenuItem value="small"> Small </MenuItem>
+        <MenuItem value="medium"> Medium </MenuItem>
+        <MenuItem value="large"> Large </MenuItem>
+      </Select>
     </div>
   );
 }
